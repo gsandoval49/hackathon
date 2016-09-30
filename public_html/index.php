fed84b933d4f3cf7771440b044274e89087abe0f
Giles Sandoval <gsandoval2277@gmail.com>
2016-09-30 13:17:10 -0600

    html ready

--- public_html/index.php
+++ public_html/index.php
@@ -1,1 +1,88 @@
-hello world
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+
+		<meta charset="UTF-8"/>
+		<meta http-equiv="X-UA-Compatible" content="IE=edge"/>
+		<meta name="viewport" content="width=device-width, initial-scale=1"/>
+
+		<!--this is for font icons or favicons-->
+		<link data-require="font-awesome@4.5.0" data-semver="4.5.0" rel="stylesheet"
+				href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.css"/>
+
+		<!-- Latest compiled and minified CSS -->
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"
+				integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+
+		<!-- FontAwesome -->
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.6.3/css/font-awesome.min.css">
+
+		<!-- animate.css -->
+		<!--<link type="text/css" href="css/animate.css" rel="stylesheet"/>-->
+
+		<!-- ADD OTHER CSS FILES HERE -->
+
+		<!-- ADD YOUR CUSTOM CSS HERE -->
+		<link rel="stylesheet" href="css/style.css" type="text/css"/>
+
+		<!-- GOOGLE FONTS -->
+		<link href="https://fonts.googleapis.com/css?family=Cabin" rel="stylesheet">
+
+		<!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+		<!--[if lt IE 9]>
+		<script type="text/javascript" src="//oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+		<script type="text/javascript" src="//oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+		<![endif]-->
+
+		<!-- jQuery -->
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/1.9.1/jquery.min.js" type="text/javascript"></script>
+
+		<!-- jQuery Form, Additional Methods, Validate -->
+		<script type="text/javascript"
+				  src="https://cdnjs.cloudflare.com/ajax/libs/jquery.form/3.51/jquery.form.min.js"></script>
+		<script type="text/javascript"
+				  src="https://cdnjs.cloudflare.com/ajax/libs/jquery-validate/1.15.0/jquery.validate.min.js"></script>
+		<script type="text/javascript"
+				  src="https://cdnjs.cloudflare.com/ajax/libs/jquery-validate/1.15.0/additional-methods.min.js"></script>
+
+		<!-- Your JavaScript Form Validator -->
+		<script src="js/form-validate.js"></script>
+
+		<!-- Latest compiled and minified JavaScript -->
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"
+				  integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa"
+				  crossorigin="anonymous"></script>
+
+
+<!--		<!--for recaptcha-->
+		<script src='https://www.google.com/recaptcha/api.js'></script>-->
+
+		<!-- ADD YOUR CUSTOM JS FILES HERE -->
+		<script src="js/custom.js" type="text/javascript"></script>
+
+
+		<title>RSI Hackathon</title>
+	</head>
+	<body class="sfooter">
+		<!-- HEADER SECTION -->
+
+		<!-- begin sticky footer -->
+		<div class="sfooter-content">
+
+			<header>
+
+
+			</header>
+
+			<!-- MAIN SECTION -->
+			<main>
+
+
+
+			</main>
+		</div> <!-- sticky footer end div -->
+		<!-- FOOTER SECTION -->
+		<footer>
+
+		</footer>
+	</body>
